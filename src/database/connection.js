f7f07a37d2953eff41fd23d1b6d4d9081629a7e9
DevWesly <dev.lucaswesley@gmail.com>
2023-01-21 13:03:31 -0300

    ..

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,8 +1,10 @@
 const Sequelize = require("sequelize");
 
 const dbConfig = require('../config/dbConfig');
-const { connect } = require("../routes");
+const Usuario = require("../models/Usuario");
 
 const connection = Sequelize(dbConfig);
+
+Usuario.init(connection);
 
 module.exports = connection;
