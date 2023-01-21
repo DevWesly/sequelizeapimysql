ee5c370f6f5e7bac38e0e93db7c7bf91858df8b1
DevWesly <dev.lucaswesley@gmail.com>
2023-01-21 13:01:45 -0300

    criando rota de usuario

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const express = require ('express');
+const UsuarioControllers = require("../src/controllers/UsuarioControllers");
 
 const routes = express.Router();
 
-routes.get('/',(req,res)=>{res.json('teste de funcionalidade')})
+routes.get('/',(req,res)=>{res.json('teste de funcionalidade')});
+routes.get('/usuarios',UsuarioControllers.listar)
 
 module.exports = routes;
