1761e26626aecc6b0c07a1fd06d41cce26f1f3d5
DevWesly <dev.lucaswesley@gmail.com>
2023-01-22 10:05:10 -0300

    criando funcionalidades para usuarios

--- src/controllers/UsuarioControllers.js
+++ src/controllers/UsuarioControllers.js
@@ -1,3 +1,27 @@
+const Usuario = require('../models/Usuario');
+
 module.exports = {
-    async listar (req,res){res.json("ja ja crio")}
+       async listarUm(req, res) {
+        let usuarioId = req.params.id;
+       let user = await Usuario.findOne({where:{id : usuarioId}});
+       if(!user){return res.json({'msg':'usuario não encontrado'})}
+        return res.json({
+            user
+        });
+    },
+    async listarTodos(req, res) {
+        let usuarios = await Usuario.findAll();
+        return res.json({
+            usuarios
+        });
+    },
+    async criar(req, res) {
+        const {nome,sobrenome,idade} = req.body;
+        await Usuario.findOrCreate({
+            where:{nome,sobrenome,idade}
+        });
+        // await Usuario.create({ nome,sobrenome,idade});
+        res.json(await Usuario.findAll())
+
+    }
 }
