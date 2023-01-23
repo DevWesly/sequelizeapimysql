52abc807e022ba72d58b1823f58f3f3e20786d8a
DevWesly <dev.lucaswesley@gmail.com>
2023-01-23 12:45:09 -0300

    importando e usando Endereco nos arquivos

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const path = require('path');
 
-const routes = require("./routes");
+const routes = require("./routes/usuarioRoutes");
+const enderecosRoutes = require("./routes/enderecoRoutes")
 
 require('./database/connection');
 
 const app = express();
 app.use(express.json());
 
-app.use(routes)
+app.use(routes);
+app.use(enderecosRoutes);
 
 const servidor = 3333
 
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const Usuario = require('../models/Usuario');
+const Endereco = require('../models/Endereco')
 
 
 const dbConfig = require('../config/dbConfig');
@@ -7,8 +8,9 @@ const dbConfig = require('../config/dbConfig');
 const connection = new Sequelize(dbConfig);
 
 Usuario.init(connection);
+Endereco.init(connection);
 
-// Usuario.associate(connection.models);   
+Usuario.associate(connection.models);   
 
 
 module.exports = connection;
