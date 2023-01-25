d5d5a4c854482ea38611471156fdfa034b28629b
DevWesly <dev.lucaswesley@gmail.com>
2023-01-25 01:04:41 -0300

    migration para tabela profissoes

---
+++ src/database/migrations/20230124040114-profissoes.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.createTable('profissoes', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+
+  },
+
+  async down(queryInterface, Sequelize) {
+
+    return queryInterface.dropTable('profissoes');
+  }
+};
