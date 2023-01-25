0a09e88509818c1ea144c6745ced58ff6b6245cb
DevWesly <dev.lucaswesley@gmail.com>
2023-01-25 01:07:07 -0300

    associando usuario com profissao

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,25 +1,30 @@
 const {
-    Model,
-    DataTypes
-  } = require('sequelize');
-  
-  class Usuario extends Model {
-    static init(sequelize) {
-      super.init({
-        nome: DataTypes.STRING,
-        sobrenome: DataTypes.STRING,
-        idade: DataTypes.INTEGER
-      }, {
-        sequelize,
-        tableName:"usuarios"
-      })
-    }
-    static associate(models) {
-      Usuario.hasMany(models.Endereco, {
-        foreignKey: "idUsuario",
-        as: "enderecos"
-      });
-    }
+  Model,
+  DataTypes
+} = require('sequelize');
+
+class Usuario extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: DataTypes.STRING,
+      sobrenome: DataTypes.STRING,
+      idade: DataTypes.INTEGER
+    }, {
+      sequelize,
+      tableName: "usuarios"
+    })
   }
-  
-  module.exports = Usuario;
+  static associate(models) {
+    Usuario.hasMany(models.Endereco, {
+      foreignKey: "idUsuario",
+      as: "enderecos"
+    });
+    Usuario.belongsToMany(models.Profissao, {
+      foreignKey: "idUsuario",
+      through: "usuarioprofissoes",
+      as: "profissao"
+    })
+  }
+}
+
+module.exports = Usuario;
